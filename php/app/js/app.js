8a4f2f81266cd84439dfb8735909445107567665
Ramesh Vijayaraghavan <rvramesh@gmail.com>
2013-09-14 12:06:41 +0530

    integrating charts

--- php/app/js/app.js
+++ php/app/js/app.js
@@ -1,4 +1,3 @@
-
 function DashboardCtrl($scope, $route, $routeParams) {
     $scope.todos = [{
         text: 'learn angular',
@@ -36,4 +35,72 @@ function DashboardCtrl($scope, $route, $routeParams) {
         alert('here');
     });
 
+    $scope.date = "2-13";
+
+    var data1 = [
+        [
+            [0, 10],
+            [1, 50],
+            [2, 20]
+        ]
+    ],
+        data2 = [
+            [
+                [0, 4],
+                [1, 2],
+                [2, 4]
+            ]
+        ],
+        curr = 1;
+
+    $scope.data = data1;
+    
+
+    $scope.change = function() {
+        if (curr === 1) {
+            $scope.data = data2;
+            curr = 2;
+        } else {
+            $scope.data = data1;
+            curr = 1;
+        }
+    };
+
 }
+
+angular.module('prsApp', [])
+// Register the 'myCurrentTime' directive factory method.
+// We inject $timeout and dateFilter service since the factory method is DI.
+.directive('barchart', function() {
+    return {
+        restrict: 'A',
+        require: '?ngModel',
+        link: function(scope, element, attr, controller) {
+
+            var options = {};
+
+            var getOptions = function() {
+                return angular.extend({}, options, scope.$eval(attr.fltBarchartoptions));
+            };
+
+            var init = function(v) {
+                if (controller) {
+                    controller.$render = function() {
+                        var temp_options = getOptions();
+                        // need to add this due to it not being parsed from string correctly - probably due to angular security
+                        Flotr.draw(element[0], v, temp_options);
+                    };
+                }
+
+                if (controller) {
+                    // Force a render to override
+                    controller.$render();
+                }
+            };
+
+            var data = scope[attr.ngModel];
+
+            scope.$watch('data', init);
+        }
+    };
+});
